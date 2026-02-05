4b9f996ce087f10625b17433b88b1c90b76be510
Clara Loforti <clara.loforti@gmail.com>
2026-02-05 13:45:09 +0100

    creo array di oggetti food da ritornare alla rotta /bacheca

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Attivazione cartella "public" per uso file statici
+app.use(express.static('public'));
+
+// Rotta index dell'APP
+app.get('/', (req, res) => {
+    res.send("<h1>Server del mio blog</h1>")
+})
+
+// Rotta bacheca
+app.get('/bacheca', (req, res) => {
+    // Dati del json da ritornare a questa rotta
+    const food = [
+        {
+            id: 1,
+            name: "Ciambellone",
+            image: "imgs/ciambellone.jpeg",
+            ingredients: ["farina", "lievito", "uova", "zucchero"],
+        }, {
+            id: 2,
+            name: "Cracker barbabietola",
+            image: "imgs/cracker_barbabietola.jpeg",
+            ingredients: ["cracker", "barbabietola"],
+        }, {
+            id: 3,
+            name: "Pane fritto",
+            image: "imgs/pane_fritto_dolce.jpeg",
+            ingredients: ["pane", "zucchero", "olio"],
+        }, {
+            id: 4,
+            name: "Pasta barbabietola",
+            image: "imgs/pasta_barbabietola.jpeg",
+            ingredients: ["pasta", "barbabietola"],
+        }, {
+            id: 5,
+            name: "Torta paesana",
+            image: "imgs/torta_paesana.jpeg",
+            ingredients: ["farina", "cacao", "burro"],
+        },
+    ]
+})
